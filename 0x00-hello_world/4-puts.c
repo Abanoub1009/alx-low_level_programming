167a87f4b73b5458191bcaa17eff403f73cddc0d
abanoub soliman <bibosoliman20@icloud.com>
2023-03-16 10:49:22 -0700

    task 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+*prints exactly "Programming is like building a multilingual puzzle
+*
+* Return: Always 0 (Success)
+*/
+int main(void){
+puts("Programming is like building a multilingual puzzle\n");
+return 0;
+}
