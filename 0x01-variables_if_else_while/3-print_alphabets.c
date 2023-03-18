51ec3269beee135ffa1237f3697acfcecd32511b
abanoub soliman <bibosoliman20@icloud.com>
2023-03-17 17:50:45 -0700

    task 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+char i;
+char y;
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
+putchar('\n');
+for (y = 'A' ; y <= 'Z' ; y++)
+putchar(y);
+putchar('\n');
+return (0);
+}
