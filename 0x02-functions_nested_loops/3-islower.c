559ee14fa2d08406f955e086a315afd12c4140ff
abanoub soliman <bibosoliman20@icloud.com>
2023-03-21 14:22:42 -0700

    fix task 4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,14 +4,15 @@
  * _islower - function to prints lower case
  *
  * @c: parameter to be printed
+ *
  * Return: 1 if it is a lower case
  * and 0 otherwise
  */
 
 int _islower(int c)
 {
-if (c >= 97 && c <= 122)
-return (1);
-else
-return (0);
+	if (c >= 97 && c <= 122)
+		return (1);
+	else 
+		return (0);
 }
