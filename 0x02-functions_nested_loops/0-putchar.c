e6a9908f589e0cbdcc6b5be43a76a49888ddfa37
abanoub soliman <bibosoliman20@icloud.com>
2023-03-21 13:16:42 -0700

    task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - main function
+ * _putchar : print the alphabets putchar
+ * Return: always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('a');
+	_putcahr('r');
+	_putchar('\n');
+	return (0);
+}
