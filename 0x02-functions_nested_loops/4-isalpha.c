3e9e3ee98d66f3752bf005e2f65ee202274fa50d
abanoub soliman <bibosoliman20@icloud.com>
2023-03-21 15:00:50 -0700

    task 5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,22 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * _isalpha - function that checks for alphabets
  *
- * Return: Always 0.
+ * @c: parameter to be printed
+ *
+ * Return: 1 if it is an alphabet
+ * and 0 overwise
  */
-int main(void)
-{
-int r;
 
-r = _isalpha('H');
-_putchar(r + '0');
-r = _isalpha('o');
-_putchar(r + '0');
-r = _isalpha(108);
-_putchar(r + '0');
-r = _isalpha(';');
-_putchar(r + '0');
-_putchar('\n');
-return (0);
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
 }
