5d2d08cdc90e222a4d08d9c017cc853ea4ba2f03
abanoub soliman <bibosoliman20@icloud.com>
2023-03-24 03:49:52 +0200

    fix

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
-
+#ifdef main.h
+#define mian.h
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -9,3 +10,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+#endif
