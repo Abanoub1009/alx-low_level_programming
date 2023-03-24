93a24e6cecad6a1706f39fe3b13b923ba4170d66
abanoub soliman <bibosoliman20@icloud.com>
2023-03-24 03:42:43 +0200

    task 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - check if a letter is upper
+ * @c: the number to be checked
+ * Return: 1 for upper letter or 0 for any else
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
