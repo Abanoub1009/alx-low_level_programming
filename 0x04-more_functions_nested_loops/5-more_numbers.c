00e7e17cf2c43ad7cabdb98257f740b690094541
abanoub soliman <bibosoliman20@icloud.com>
2023-03-24 09:46:02 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ * Return: always 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int z;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		for (z = 0 ; z <= 14 ; z++)
+		{
+			if (z >= 10)
+				_putchar(z /10 + '0');
+			_putchar(z % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
