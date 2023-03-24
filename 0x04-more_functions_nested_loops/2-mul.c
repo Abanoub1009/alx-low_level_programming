4ea95ea2d24a7f773c97c2b3bedf31e5860486c7
abanoub soliman <bibosoliman20@icloud.com>
2023-03-24 08:16:43 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 int
+ * @a: first int
+ * @b: secound int
+ * Return: addition of the multiplaction
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
