c6e56dac46981d6fc468bd9c1047816ab5acba34
abanoub soliman <bibosoliman20@icloud.com>
2023-03-27 12:22:36 -0700

    task 10

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy -  that copies the string pointed to by src to dest
+ * @src: the first string
+ * @dest: the secound string
+ * Return: void
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+		return (dest);
+}
