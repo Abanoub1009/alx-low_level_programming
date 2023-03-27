c8e0409c4d2d70ee1621cd0cb839f47ad8ea1e55
abanoub soliman <bibosoliman20@icloud.com>
2023-03-27 11:18:14 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - that returns the length of a string
+ *
+ * @s: name of the string
+ * Return: nothing
+ */
+
+int _strlen(char *s)
+{
+	int i = strlen(s);
+
+	return (i);
+}
