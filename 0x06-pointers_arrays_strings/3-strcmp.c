8660cc4f88a0221eede6a5ebb341246f46077094
abanoub soliman <bibosoliman20@icloud.com>
2023-03-29 13:45:56 -0700

    task 4

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,16 +11,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
-{
-s1++;
-s2++;
-}
-
-if (*s1 == *s2)
-return 0;
-else if (*s1 < *s2)
-return (-15);
-else
-return (15);
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else if (*s1 < *s2)
+		return (-1);
+	else
+		return (1);
 }
