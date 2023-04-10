70a0916cc21fd4eec0abd098def75c63e0cfbda7
abanoub soliman <bibosoliman20@icloud.com>
2023-04-10 10:12:25 -0700

    task 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: the number of argumentes
+ * @argv: the string
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
