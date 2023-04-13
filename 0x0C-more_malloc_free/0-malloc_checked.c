3aecbf86888d791e671809fdf7caf4db738a66d9
abanoub soliman <bibosoliman20@icloud.com>
2023-04-13 09:56:39 -0700

    task 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * malloc_checked - that allocates memory using malloc.
+ * @b: the integer
+ * Return: void
+ */
 
 void *malloc_checked(unsigned int b)
 {
