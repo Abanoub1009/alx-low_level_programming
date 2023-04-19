1e957bb016947a35881b573ef916aa5fd131ef6b
abanoub soliman <bibosoliman20@icloud.com>
2023-04-18 18:11:21 -0700

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog's information
+ * @name: the first member
+ * @age: the scound member
+ * @owner: the third member
+ *
+ * description: long
+ */
+
 struct dog
 {
 	char *name;
