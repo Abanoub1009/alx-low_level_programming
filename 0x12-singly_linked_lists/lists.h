78351b04b25a6431ac4142cee060a9c6e540d007
abanoub soliman <bibosoliman20@icloud.com>
2023-04-27 18:36:02 -0700

    task 1

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,5 @@
-#include <stddef.h>
 #ifndef LISTS_H
 #define LISTS_H
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -12,10 +10,11 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
+#include <stddef.h>
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
