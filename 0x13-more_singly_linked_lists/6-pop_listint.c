55651c6c9e8fd29edad5246b36bab0f928504e7a
abanoub soliman <bibosoliman20@icloud.com>
2023-05-03 02:44:15 +0300

    task 7

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,12 +8,12 @@
  */
 int pop_listint(listint_t **head)
 {
+	int n = (*head)->n;
+	listint_t *temp = *head;
+
 	if (head == NULL || *head == NULL)
 		return (0);
 
-	int n = (*head)->n;
-
-	listint_t *temp = *head;
 	*head = (*head)->next;
 	free(temp);
 
