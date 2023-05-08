00a55b1461e252e5cf690e6a6433ef84ebb0835f
abanoub soliman <bibosoliman20@icloud.com>
2023-05-08 15:54:53 -0700

    task 1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: Name of the file to read
+ * @letters: Number of letters to read and print
+ *
+ * Return: Number of letters it could read and print, 0 if failed
+ */
+size_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t n_read, n_written;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+
+	n_read = read(fd, buf, letters);
+	close(fd);
+	if (n_read == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	n_written = write(STDOUT_FILENO, buf, n_read);
+	free(buf);
+	if (n_written == -1 || n_written != n_read)
+		return (0);
+
+	return (n_written);
+}
