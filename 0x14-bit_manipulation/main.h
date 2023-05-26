4134b791b9ee5db53f94f9325aef55e9f4884d96
abanoub soliman <bibosoliman20@icloud.com>
2023-05-26 14:34:49 -0700

    task 7

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 #include <stddef.h>
+
+int get_endianness(void);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
